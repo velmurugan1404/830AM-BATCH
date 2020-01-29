58524b73c80fd2201e7138e884a2f36801e258c4
velmurugan1404 <vm1404@gmail.com>
2020-01-29 10:18:47 +0530

    new code added

--- src/org/bank/det/RBI.java
+++ src/org/bank/det/RBI.java
@@ -2,13 +2,15 @@ package org.bank.det;
 
 public class RBI {
  public void adharCarddetail() {
-	System.out.println("ADHAR CARD NUMBER IS 1234");
+	System.out.println("ADHAR CARD NUMBER IS 987654");
 
 }
 
- public void panCarddetails() {
-	System.out.println("PAN CARD NUMBER IS 98765");
+
  }
 
-}
+	
+
+
+
 
